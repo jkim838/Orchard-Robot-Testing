aebe1613ff34574a7c9b69256e6f568be57ef41c
jkim838 <43252359+jkim838@users.noreply.github.com>
2019-02-05 15:40:43 +1300

    05.02.19
    changed publisher type to Float32
    minor code changes including structures.

--- FCMonitor/src/FCMonitor.cpp
+++ FCMonitor/src/FCMonitor.cpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 #include <geometry_msgs/Vector3.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Float32.h>
 #include <std_msgs/UInt32.h>
 #include <vector>
 #include <iostream>
@@ -17,6 +18,9 @@ sensor_msgs::Image image_depth_array;
 unsigned int height_center;
 unsigned int width_center;
 unsigned int maximum_depth;
+
+float test_value = 99.99;
+std_msgs::Float32 test_variable; // casted target location in mm for messages.
 
 float calculate_width(){
 
@@ -40,30 +44,35 @@ void object_position(const std_msgs::Float32MultiArray::ConstPtr& data){
     for (int i = 1; i < arr_length - 1; i+=3){
       // For realistic purposes, give 10px margin between center point of the y-axis...
       if((const unsigned) array.data[i] >= height_center- CENTER_MARGIN && (const unsigned) array.data[i] <= height_center + CENTER_MARGIN){
-        std::cout << "Located!" << std::endl;
+        std::cout << "Located within the margin!" << std::endl;
 
         // we assume the viewing distance (horizontal) is 1700mm at 925mm between camera surface and the canopy.
         // because image is 640px wide, each pixel represents 2.66mm
-        float px_mm = calculate_width() / 640;
+        float width_mm = calculate_width(); // calculate the viewing width
+        float px_mm = width_mm / 640; // mm per pxiel
         // convert x-coordinate to mm...
-        float x_mm = array.data[i-1] * px_mm;
-        std::cout << "X in mm: " << x_mm << std::endl;
-        // to determine the z-position of the target, use the maximum depth (maximum distance away from the camera)
-        // remeber that depth is stored as an array... therefore we need to go through all the array, find the maximum distance.
+        float x_mm = array.data[i-1] * px_mm; // target location in mm (x-axis)
+        std::cout << "Target is " << x_mm << " mm away from the left corner." << std::endl;
+
+        // x_mm information is returned and used somewhere else.
+        // array.data[i-1] = x_mm;
       }
     }
   }
 }
 
 void image_size(const sensor_msgs::Image::ConstPtr& image_data){
+
   // Subscribed to topic "image", obtain the dimension of the image (default: 640*480px)...
   height_center = image_data->height;
   width_center = image_data->width;
   // Determine the center point of x,y-axis...
   height_center /=2;
   width_center /=2;
+
 }
 
+/*
 void image_depth(const sensor_msgs::Image::ConstPtr& image_depth_data){
   image_depth_array.data = image_depth_data->data;
   std::cout << "Image Width: " << image_depth_data->width << std::endl;
@@ -72,22 +81,24 @@ void image_depth(const sensor_msgs::Image::ConstPtr& image_depth_data){
   // Iterate throgh entire array of depth...
   //for(int i = 0; i < )
 }
+*/
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "FC_control_node");
   ros::NodeHandle FC_nh;
   // Subscriber Declaration...
   ros::Subscriber FC_coordinate_sub = FC_nh.subscribe("/position", 10, object_position);
-  ros::Subscriber FC_array_size_sub = FC_nh.subscribe("/debug_image", 10, image_size);
+  ros::Subscriber FC_array_size_sub = FC_nh.subscribe("/debug_image", 10, image_size); // this is running multiple times?
   // ros::Subscriber FC_depth_info_sub = FC_nh.subscribe("/camera/depth/image_rect_raw", 10, image_depth);
+
   // Publisher Declaration...
-  ros::Publisher FC_pub = FC_nh.advertise<std_msgs::Float32MultiArray>("/ObjectPosition",10);
+  ros::Publisher FC_pub = FC_nh.advertise<std_msgs::Float32>("/LaserNumber",10);
   ros::Rate rate(10);
 
 
   while(ros::ok()){
     ros::spinOnce();
-    FC_pub.publish(array);
+    FC_pub.publish(test_variable);
     rate.sleep();
   }
 }
